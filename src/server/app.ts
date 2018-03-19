c9e8e78babed34fea1d61b29a20d07189c109779
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2018-03-19 22:02:34 +0100

    Setup static file server for /public

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,28 @@
+import * as path from "path";
+
 import * as Koa from "koa";
 import * as Router from "koa-router";
+import * as send from "koa-send";
+
 import GraphqlRouter from "./graphql-router";
 
 const app = new Koa();
 const router = new Router();
 
+// Note: We want "build/public" or "public", relative to the built "server.js" file,
+// not a path relative to the source, which is what webpack gives for __dirname
+const publicPath = path.join(
+  path.dirname(process.mainModule.filename),
+  "public"
+);
+
+// Static file handling:
+//    Implemented this was as koa-static doesn't play nicely with webpack / dist
+router.get("/public/:file+", async ctx => {
+  await send(ctx, ctx.params.file, { root: publicPath });
+});
+
+// GraphQL handling:
 router.use("/graphql", GraphqlRouter.routes(), GraphqlRouter.allowedMethods());
 
 router.get("/", async ctx => {
