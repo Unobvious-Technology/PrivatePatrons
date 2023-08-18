8fb88cbb45f518f08b8955673f83621a8489aed2
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2023-08-18 21:15:08 +0200

    Remove twitter

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,9 +23,6 @@ export default function Home() {
           property="og:image"
           content="https://privatepatrons.com/og-image.png"
         />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@privatepatrons" />
-        <meta name="twitter:creator" content="@unobvioustech" />
         <link
           href="https://fonts.googleapis.com/css?family=Open+Sans:400,700"
           rel="stylesheet"
